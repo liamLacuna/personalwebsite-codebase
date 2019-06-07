527ed36d58e1c2ec438cffaee73ba6a37e97814c
Liam <lon.liam.li@gmail.com>
2019-06-06 17:52:42 -0700

    font awesome bug

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 export default class Footer extends Component{
   render() {
     return (
@@ -7,9 +9,8 @@ export default class Footer extends Component{
         <div className="row">
           <div className="twelve columns">
             <ul className="social-links">
-              <li><a href="https://www.linkedin.com/in/yangliliam/"><i className="fa fa-linkedin" /></a></li>
-              <li><a href="https://www.instagram.com/liamthecoder/"><i className="fa fa-instagram" /></a></li>
-              <li><a href="https://github.com/liamLacuna"><i className="fa fa-github" /></a></li>
+              <li><a href="https://www.linkedin.com/in/yangliliam/"><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
+              <li><a href="https://github.com/liamLacuna"><FontAwesomeIcon icon={faGithub} /></a></li>
             </ul>
             <ul className="copyright">
               <li>© Copyright 2019 Yang(Liam) Li</li>
